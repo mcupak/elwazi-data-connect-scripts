f9ed6af9550c3363d3f4e81e4e97550cde5b1082
Miro Cupak <miro@dnastack.com>
2023-07-06 15:24:53 -0400

    Update Trino CLI

--- install-trino-cli.sh
+++ install-trino-cli.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl -L -o trino https://repo1.maven.org/maven2/io/trino/trino-cli/369/trino-cli-369-executable.jar
+curl -L -o trino https://repo1.maven.org/maven2/io/trino/trino-cli/420/trino-cli-420-executable.jar
 mv trino /usr/local/bin
 chmod +x /usr/local/bin/trino
